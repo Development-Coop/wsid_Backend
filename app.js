0d0f92747148b92e4b3afc87193e0dc0722a32a1
Durairaj <durairaj.web@gmail.com>
2024-09-13 19:56:23 +0530

    netlify config

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const serverless = require('serverless-http');
 const routes = require('./routes/index');
 const app = express();
 
@@ -9,7 +10,13 @@ app.use(express.json()); // To handle JSON request bodies
 // Routes
 app.use('/api', routes);
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// Export the app wrapped in serverless-http
+module.exports.handler = serverless(app);
+
+// If running locally, start the app on port 3000
+if (process.env.NODE_ENV !== 'production') {
+  const port = process.env.PORT || 3000;
+  app.listen(port, () => {
+    console.log(`Server running locally on port ${port}`);
+  });
+}
