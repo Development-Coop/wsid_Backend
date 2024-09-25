88e68a3c288663cd7d84dce9fd5056cf66c10f58
Durairaj <durairaj.web@gmail.com>
2024-09-26 00:42:40 +0530

    error message update

--- constants/messages.js
+++ constants/messages.js
@@ -27,10 +27,10 @@ module.exports = {
   APPLE_SIGNIN_SUCCESS: 'Google Sign-In successful.',
   APPLE_SIGNIN_FAILED: 'Google Sign-In successful.',
   SUCCESS: 'success',
-  ACCESS_TOKEN_REQUIRED: 'Access Token Required',
-  EXPIRED_TOKEN: 'Invalid or Expired Token',
-  EXPIRED_REFRESH_TOKEN: 'Invalid or Expired Refresh Token',
-  INVALID_REFRESH_TOKEN: 'Invalid Refresh Token',
-  REFRESH_TOKEN_ERROR: 'Refresh token has expired. Please log in again.',
-  USER_NOT_FOUND: 'User not found'
+  ACCESS_TOKEN_REQUIRED: 'Access Token Required!',
+  EXPIRED_TOKEN: 'Invalid or Expired Token!',
+  EXPIRED_REFRESH_TOKEN: 'Invalid or Expired Refresh Token!',
+  INVALID_REFRESH_TOKEN: 'Invalid Refresh Token!',
+  REFRESH_TOKEN_ERROR: 'Refresh token has expired! Please log in again.',
+  USER_NOT_FOUND: 'User not found!'
 };
