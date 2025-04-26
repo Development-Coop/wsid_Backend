f3f6604536064fa46573bf8f1273a4d3882e0168
Arjun Pandi <gmarjun100@gmail.com>
2025-04-26 21:42:07 +0530

    cors adjust fix

--- app.js
+++ app.js
@@ -6,8 +6,25 @@ const routes = require("./routes/index");
 const app = express();
 
 // CORS config
-// const allowedOrigins = ['http://localhost:9000'];
-app.use(cors());
+const allowedOrigins = ["https://wsid.com", "http://localhost:9000"];
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps or curl requests)
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 // Middleware
 app.use(express.json());
